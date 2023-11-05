e9ad7d33bffd5c5721f0519bc3718b93c81634b5
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-11-05 21:39:18 +0530

    Create index.js

---
+++ src/apis/controllers/v1/updateStatus1/index.js
@@ -0,0 +1,18 @@
+// const StudentService = require('@root/src/apis/services/v1/updateStatus');
+const { HttpResponseHandler } = require('intelli-utility');
+const StudentService = require('@services/v1/updateStatus1');
+// Controller function to update a teacher by ID
+const updatestatus = async (req, res) => {
+  try {
+    const data = await StudentService.updateStudentStatus(req.params.id, req.body);
+    if (!data) {
+      return HttpResponseHandler.success(req, res, data);
+    }
+    return HttpResponseHandler.success(req, res, data);
+  } catch (error) {
+  //  next(error)
+  }
+};
+module.exports = {
+  updatestatus,
+};
